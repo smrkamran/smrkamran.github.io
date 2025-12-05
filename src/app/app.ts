cd5c67e3bc935b4935fa0dcbfbcadf27dd1d94a8
Sameer Kamran <sameer.kamran@schwarm.com>
2025-12-06 00:15:49 +0100

    changed years

--- src/app/app.ts
+++ src/app/app.ts
@@ -55,7 +55,7 @@ export class App {
 
   readonly statements = {
     personal:
-      'Software Engineer with more than 3 years of experience in industry. Worked as a lone-wolf as well as in Agile Teams. Expertise is design and development of Frontend Applications as well as WebApis from scratch. Also designed databases and have been part of requirement gathering teams. Always open to learn new tools and technologies.',
+      'Software Engineer with more than 6+ years of experience in industry. Worked as a lone-wolf as well as in Agile Teams. Expertise is design and development of Frontend Applications as well as WebApis from scratch. Also designed databases and have been part of requirement gathering teams. Always open to learn new tools and technologies.',
   };
 
   readonly blogLink = 'https://blog.sameerkamran.com';
